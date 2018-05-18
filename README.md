471c120d485f84cf4486214666317c03ddb4f434
zhouyu <yu.zhou@61topbaby.com>
2018-05-18 19:04:32 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Part III
+
---
+++ src/com/test/jdbctemplate/JdbcTemplateTest.java
@@ -0,0 +1,21 @@
+package com.test.jdbctemplate;
+
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class JdbcTemplateTest {
+	@Test
+	public void test() {
+		//1.创建数据源(spring提供的)
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/test_spring");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		//2.创建jdbcTemplate模板类
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);	//jdbcTemplate.setDataSource(dataSource)
+		//3.执行语句
+		jdbcTemplate.execute("create table test1(id int,name varchar(20),price decimal(10,2))");
+	}
+}
